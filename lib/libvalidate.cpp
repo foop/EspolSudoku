65b7cd756525418357fa00379904da41c170a0cf
Dominik Danter <dominik@foop.at>
2013-06-15 09:50:38 -0500

    added untestet validation helper function

---
+++ lib/libvalidate.cpp
@@ -0,0 +1,30 @@
+#include "libvalidate.h"
+
+libValidate::libValidate()
+{
+
+}
+
+//TODO silly name
+std::list<std::list<int> > findDouble(int field[]) {
+    // first save result here
+    std::list<int> occurrences[9];
+    // return this list
+    std::list<std::list<int> > packedOccurrences;
+
+    for (int i = 0; i < sizeof(field); i++) {
+        // out of range value is assumed to be non-existent
+        if (field[i] > 0 && field[i] < 10) {
+            occurrences[field[i]].push_back(i);
+        }
+    }
+
+    for (int i = 0; i < sizeof(occurrences); i++) {
+        if (occurrences[i].size() > 1) {
+            packedOccurrences.push_back(occurrences[i]);
+        }
+    }
+    return packedOccurrences;
+}
+
+
---
+++ lib/libvalidate.h
@@ -0,0 +1,13 @@
+#ifndef LIBVALIDATE_H
+#define LIBVALIDATE_H
+
+#include <list>
+
+class libValidate
+{
+public:
+    libValidate();
+    std::list<std::list<int> > findDouble(int field[]);
+};
+
+#endif // LIBVALIDATE_H
